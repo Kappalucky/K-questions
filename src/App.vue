4324b3f3b2a2e72202cdb8011c7c1a823c4f4b6a
Kappalucky <Shaquille.j.foster@gmail.com>
2021-12-13 20:02:54 -0500

    Added Vue and basic scaffolding. Added Tailwind as well. Will start basic set up of templating

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Questions/>
+  <h1>{{title}}</h1>
+  <div v-if="test">
+    <ol v-for="(question, index) in test" :key="index">
+    {{question.question}}
+    <li v-for="(choice, i) in choices[index]" :key="i">
+    {{choice}}
+    </li>
+    </ol>
+    <button>Submit</button>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+<script setup lang="ts">
+import { ref, reactive, onMounted, computed } from "vue";
+import Questions from "./components/Questions.vue";
+import questions from '@/assets/questions.json';
 
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+const test = ref([] as any)
+const title = ref('');
+
+const choices = computed(() => {
+  return test.value.map((question: any) => {
+    return question.choices;
+  })
+})
+
+
+onMounted(() => {
+  test.value = questions.questions
+  title.value = questions.title;
+})
 </script>
 
 <style>
