bd0de1735741c8050d41e69a1f4c8844bc560c2c
Kappalucky <Shaquille.j.foster@gmail.com>
2021-12-15 13:04:49 -0500

    added array to keep track of unanswered answers and added conditional checks

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <Questions/>
-  <h1>{{title}}</h1>
+  <h1>{{titleState}}</h1>
   <div v-if="questionsState">
-    <form action="#">
+    <form @submit.prevent="handleSubmit">
       <div class="shadow overflow-hidden sm:rounded-md">
         <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
           <fieldset v-for="(question, index) in questionsState" :key="index" :id="'Q'+index">
@@ -22,7 +22,7 @@
         </div>
         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
           <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Save
+            Submit
           </button>
         </div>
       </div>
@@ -31,13 +31,14 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive, onMounted, computed } from "vue";
+import { ref, reactive, onMounted, computed, unref } from "vue";
 import Questions from "./components/Questions.vue";
 import {questions as Q, title} from '@/assets/questions.json';
 
 const questionsState = ref([] as any)
-const titleState = ref('');
+const titleState = ref(title);
 const answers = ref([] as any)
+const unanswered = ref([] as any)
 
 const choices = computed(() => {
   return questionsState.value.map((question: any) => {
@@ -45,10 +46,38 @@ const choices = computed(() => {
   })
 })
 
+const handleSubmit = () => {
+  //?: No need to do any array validation as the answers array is dynamically filled to the length of the state
+  //TODO: If any of the answers in the answers array is blank, add error class to panel relating to index
+  //TODO: If everything is good, call grading function
+  const errorCheck = [] as any;
+
+  for (let index = 0; index < unref(answers).length; index++) {
+    const element = unref(answers)[index];
+    if (element === '') {
+      errorCheck.push(index);
+    }
+  }
+
+  if (errorCheck.length != 0) {
+    console.log(errorCheck)
+    console.log('Answer all questions before submitting. Unanswered questions are displayed in yellow.')
+  } else {
+    console.log('sent');
+  }
+}
+
+const grading = () => {
+  //TODO: Need to check each answer in answers array against questionState.answer based on index
+  //TODO: If values do not match, add error class and messages to panel
+  //TODO: Highlight correct answer based on index of question
+  //TODO: If correct, add success class and message to panel
+}
+
 
 onMounted(() => {
   questionsState.value = Q
-  titleState.value = title;
+  //titleState.value = title;
   questionsState.value.forEach(question => {
     answers.value.push('');
   });
