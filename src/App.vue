e43ecc325f04b755c21a78c78a9841066f2d5d72
Kappalucky <Shaquille.j.foster@gmail.com>
2021-12-15 17:07:40 -0500

    added yellow ring conditionally...is not perfect but will iron out bugs later on

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Questions/>
-  <h1>{{titleState}}</h1>
+<div class="min-h-full mt-6">
+  <!--<Questions/>-->
+  <h1 class="text-2xl font-semibold">{{titleState}}</h1>
   <div v-if="questionsState">
     <form @submit.prevent="handleSubmit">
       <div class="shadow overflow-hidden sm:rounded-md">
         <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
-          <fieldset v-for="(question, index) in questionsState" :key="index" :id="'Q'+index">
+          <fieldset
+            v-for="(question, index) in questionsState"
+            :key="index" :id="'Q'+index"
+            class="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-2 xs:max-w-lg md:max-w-full sm:mx-auto rounded-lg sm:px-10"
+            :class="{'ring-gray-900/5' : answers[index] !== '', 'ring-yellow-400' : index === unanswered[index]}"
+            >
             <div>
-              <legend class="text-base font-medium text-gray-900">{{question.question}}</legend>
+              <legend class="text-left font-medium text-gray-900"><span>{{index + 1}}.&nbsp;</span>{{question.question}}</legend>
             </div>
             <div class="mt-4 space-y-4">
               <div class="flex items-center" v-for="(choice, i) in choices[index]" :key="i">
@@ -20,7 +25,7 @@
             </div>
           </fieldset>
         </div>
-        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+        <div class="px-4 py-3 text-center sm:px-6 text-base leading-7 font-semibold">
           <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Submit
           </button>
@@ -28,6 +33,8 @@
       </div>
     </form>
   </div>
+</div>
+
 </template>
 
 <script setup lang="ts">
@@ -41,7 +48,7 @@ const answers = ref([] as any)
 const unanswered = ref([] as any)
 
 const choices = computed(() => {
-  return questionsState.value.map((question: any) => {
+  return unref(questionsState).map((question: any) => {
     return question.choices;
   })
 })
@@ -50,17 +57,30 @@ const handleSubmit = () => {
   //?: No need to do any array validation as the answers array is dynamically filled to the length of the state
   //TODO: If any of the answers in the answers array is blank, add error class to panel relating to index
   //TODO: If everything is good, call grading function
+  unanswered.value = [];
   const errorCheck = [] as any;
 
   for (let index = 0; index < unref(answers).length; index++) {
     const element = unref(answers)[index];
     if (element === '') {
       errorCheck.push(index);
+      unref(unanswered).push(index);
     }
   }
 
   if (errorCheck.length != 0) {
+    errorCheck.forEach(item => {
+      const itemCheck = document.getElementById(`Q${item}`);
+      if (itemCheck) {
+
+        //TODO: These contradict the conditional template classes when removing class so have to check again when cleaning up
+        //itemCheck.classList.remove('ring-gray-900/5');
+        itemCheck.classList.add('ring-yellow-400');
+      }
+      console.log(itemCheck);
+    });
     console.log(errorCheck)
+    //TODO: Return error and change ring color yellow for each item in errorCheck
     console.log('Answer all questions before submitting. Unanswered questions are displayed in yellow.')
   } else {
     console.log('sent');
@@ -90,9 +110,7 @@ const grading = () => {
 onMounted(() => {
   questionsState.value = Q
   //titleState.value = title;
-  questionsState.value.forEach(question => {
-    answers.value.push('');
-  });
+  unref(questionsState).forEach(() => answers.value.push(''));
 })
 </script>
 
@@ -103,6 +121,5 @@ onMounted(() => {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
