0ad6c06ae7f00c45947566d4f381cee5c113d640
Kappalucky <Shaquille.j.foster@gmail.com>
2021-12-16 15:52:24 -0500

    finished error check functions and fixed issue with styling overlaps. Began refactoring and code clean up. Have to add styling to grading functionality and overall template styles per conditional rendering.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 <div class="min-h-full mt-6">
   <!--<Questions/>-->
   <h1 class="text-2xl font-semibold">{{titleState}}</h1>
+  <template v-if="results.number !== null">
+    <h4>Results</h4>
+    <p>You got {{results.number}} / {{questionsState.length}} questions correct</p>
+    <p>{{results.percentage}}<span>&nbsp;%</span></p>
+  </template>
   <div v-if="questionsState">
     <form @submit.prevent="handleSubmit">
       <div class="shadow overflow-hidden sm:rounded-md">
@@ -10,7 +15,7 @@
             v-for="(question, index) in questionsState"
             :key="index" :id="'Q'+index"
             class="relative px-6 pt-10 pb-8 bg-white shadow-xl ring-2 xs:max-w-lg md:max-w-full sm:mx-auto rounded-lg sm:px-10"
-            :class="{'ring-gray-900/5' : answers[index] !== '', 'ring-yellow-400' : index === unanswered[index]}"
+            :class="[answers[index] !== '' ? 'ring-gray-900/5' : '']"
             >
             <div>
               <legend class="text-left font-medium text-gray-900"><span>{{index + 1}}.&nbsp;</span>{{question.question}}</legend>
@@ -25,8 +30,14 @@
             </div>
           </fieldset>
         </div>
+        <p v-if="pageError.active">{{pageError.message}}</p>
         <div class="px-4 py-3 text-center sm:px-6 text-base leading-7 font-semibold">
-          <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button
+          type="submit"
+          :disabled="results.number !== null"
+          class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          :class="[results.number !== null ? 'bg-gray-600 hover:bg-gray-700' : 'bg-indigo-600 hover:bg-indigo-700']"
+          >
             Submit
           </button>
         </div>
@@ -34,18 +45,29 @@
     </form>
   </div>
 </div>
-
 </template>
 
 <script setup lang="ts">
+//* System imports
 import { ref, reactive, onMounted, computed, unref } from "vue";
-import Questions from "./components/Questions.vue";
 import {questions as Q, title} from '@/assets/questions.json';
 
-const questionsState = ref([] as any)
+//* Local components
+import Questions from "./components/Questions.vue";
+
+//* Initialize variables & functions
+const questionsState = ref(Q as any)
 const titleState = ref(title);
 const answers = ref([] as any)
 const unanswered = ref([] as any)
+const pageError = reactive({
+  message: '',
+  active: false
+});
+const results = reactive({
+  number: null as null | number,
+  percentage: null as null | number
+})
 
 const choices = computed(() => {
   return unref(questionsState).map((question: any) => {
@@ -54,62 +76,54 @@ const choices = computed(() => {
 })
 
 const handleSubmit = () => {
-  //?: No need to do any array validation as the answers array is dynamically filled to the length of the state
-  //TODO: If any of the answers in the answers array is blank, add error class to panel relating to index
-  //TODO: If everything is good, call grading function
+  /* If answer relating to position of answers array is empty, add error and styling */
+
+  //* Clears unanswered array on every call before checking answers to avoid joining multiple calls
   unanswered.value = [];
-  const errorCheck = [] as any;
 
   for (let index = 0; index < unref(answers).length; index++) {
-    const element = unref(answers)[index];
-    if (element === '') {
-      errorCheck.push(index);
+    const answer = unref(answers)[index];
+    if (answer === '') {
       unref(unanswered).push(index);
     }
   }
 
-  if (errorCheck.length != 0) {
-    errorCheck.forEach(item => {
-      const itemCheck = document.getElementById(`Q${item}`);
-      if (itemCheck) {
-
-        //TODO: These contradict the conditional template classes when removing class so have to check again when cleaning up
-        //itemCheck.classList.remove('ring-gray-900/5');
-        itemCheck.classList.add('ring-yellow-400');
-      }
-      console.log(itemCheck);
+  if (unref(unanswered).length != 0) {
+    //* For every index within unanswered array, add error styling to appropriate html field
+    unref(unanswered).forEach((index: number) => {
+      const itemCheck = document.getElementById(`Q${index}`);
+      itemCheck ? itemCheck.classList.add('ring-yellow-400') : '';
     });
-    console.log(errorCheck)
-    //TODO: Return error and change ring color yellow for each item in errorCheck
-    console.log('Answer all questions before submitting. Unanswered questions are displayed in yellow.')
+
+    pageError.message = 'Answer all questions before submitting. Unanswered questions are displayed in yellow.';
+    pageError.active = true;
+
   } else {
-    console.log('sent');
+    pageError.message = '';
+    pageError.active = false;
     grading();
   }
 }
 
 const grading = () => {
-  //TODO: Need to check each answer in answers array against questionState.answer based on index
+  /* Each answer in answers array is checked against questionState.answer. Values are calculated based on returned count */
   //TODO: If values do not match, add error class and messages to panel
   //TODO: Highlight correct answer based on index of question
   //TODO: If correct, add success class and message to panel
   let rightAnswerCount = 0;
 
   for (let index = 0; index < unref(answers).length; index++) {
-    const element = unref(answers)[index];
-    if (element === unref(questionsState)[index].answer) {
+    const answer = unref(answers)[index];
+    if (answer === unref(questionsState)[index].answer) {
       rightAnswerCount = rightAnswerCount + 1;
     }
   }
 
-  console.log(`you got ${rightAnswerCount} out of ${unref(questionsState).length} questions correct`)
-  console.log('percentage: ', (rightAnswerCount/unref(questionsState).length) * 100, '%');
+  results.number = rightAnswerCount;
+  results.percentage = (rightAnswerCount / unref(questionsState).length) * 100;
 }
 
-
 onMounted(() => {
-  questionsState.value = Q
-  //titleState.value = title;
   unref(questionsState).forEach(() => answers.value.push(''));
 })
 </script>
